3b44de5622b88f54404314c7aeb45673f19a693a
hyukkyo <shk5644@gmail.com>
2023-04-04 16:50:16 +0900

    4

--- _12949/src/Main.java
+++ _12949/src/Main.java
@@ -1,6 +1,17 @@
 class Solution {
     public int[][] solution(int[][] arr1, int[][] arr2) {
-        int[][] answer = {};
+        int[][] answer = new int[arr1.length][arr2[0].length];
+
+        for(int i=0; i<arr1.length; i++) {
+            for(int j=0; j<arr2[0].length; j++) {
+                int sum = 0;
+                for(int k=0; k<arr2.length; k++) {
+                    sum += arr1[i][k] * arr2[k][j];
+                }
+                answer[i][j] = sum;
+            }
+        }
+
         return answer;
     }
 }
@@ -10,6 +21,23 @@ public class Main {
 
         Solution s = new Solution();
 
-        int arr1 = {{1,4}, {3,2}, {4,1}};
+        int[][] arr1 = {
+                {1,4},
+                {3,2},
+                {4,1}
+        };
+        int[][] arr2 = {
+                {3,3},
+                {3,3}
+        };
+
+        int[][] answer = s.solution(arr1, arr2);
+
+        for(int i=0; i<answer.length; i++) {
+            for(int j=0; j<answer[i].length; j++) {
+                System.out.print(answer[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
