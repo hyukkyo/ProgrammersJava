c1cb34a035a7a7df55d34c75a54a1b173065566d
hyukkyo <shk5644@gmail.com>
2023-04-04 15:37:45 +0900

    3

---
+++ _12949/src/Main.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = {};
+        return answer;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Solution s = new Solution();
+
+        int arr1 = {{1,4}, {3,2}, {4,1}};
+    }
+}
