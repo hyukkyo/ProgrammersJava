f0ffa421003695103f5cca48c10043121a2ce89f
hyukkyo <shk5644@gmail.com>
2023-04-06 13:52:47 +0900

    챕터4 문자열 문제5 솔루션

--- _12932/src/Main.java
+++ _12932/src/Main.java
@@ -1,11 +1,21 @@
 class Solution {
     public int[] solution(long n) {
+//        String str = Long.toString(n);
+//        int[] answer = new int[str.length()];
+//        for(int i=0; i<answer.length; i++) {
+//            answer[i] = str.charAt(str.length() - i - 1) - '0';
+//        }
+//        return answer;
+
         String str = Long.toString(n);
-        int[] answer = new int[str.length()];
-        for(int i=0; i<answer.length; i++) {
-            answer[i] = str.charAt(str.length() - i - 1) - '0';
+        String reversed = new StringBuilder(str).reverse().toString();
+        char[] arr = reversed.toCharArray();
+
+        int[] result = new int[arr.length];
+        for(int i=0; i<result.length; i++) {
+            result[i] = arr[i] = '0';
         }
-        return answer;
+        return result;
     }
 }
 
